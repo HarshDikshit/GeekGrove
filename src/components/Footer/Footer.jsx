858492d5af61b8212c3fc968d6da3aa5939b71f5
HarshDikshit <93562022+HarshDikshit@users.noreply.github.com>
2024-05-29 22:17:32 +0530

    footer fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,7 @@ import { serverTimestamp } from 'firebase/firestore';
 import authService from '../../Firebase/auth';
 import { v4 } from 'uuid';
 import Loading from '../Loading';
+import {FaSquareFacebook, FaSquareTwitter, FaSquareInstagram, FaSquareYoutube} from 'react-icons/fa6'
 
 
 
@@ -75,30 +76,32 @@ function Footer() {
 return (
     <>
 <div className=' items-end w-full  justify-evenly
-overflow-hidden  bg-gray-400 border border-t-2 border-t-gray-300'>
+overflow-hidden bg-slate-800 border border-t-2 border-t-gray-300'>
 
 {/* main div */}
-<div className=' flex md:flex-nowrap flex-wrap items-center justify-center m-auto'>
+<div className=' flex md:flex-nowrap flex-wrap items-center justify-between m-auto'>
 
 {/* left part */}
 <div className=" w-full flex flex-col  justify-center pb-10">
 
   {/* item */}
 
-<div className=' m-auto'>
+<div>
       {/* heading */}
   <div className=" flex flex-col w-full mb-3 px-8 md:px-40  justify-center">
 
-    <div className=' flex flex-col justify-center '>
-    <h1 className=' justify-self-center text-gray-800 uppercase font-bold text-2xl my-2'>Technologies Used</h1>
-    <div className=' bg bg-yellow-400 w-[250px] h-[3px] justify-self-center'></div>
+    <div className=' flex flex-col justify-center items-center'>
+    <h1 className=' justify-self-center text-gray-300 uppercase font-bold text-2xl my-2'>About</h1>
+    <div className=' bg bg-yellow-400 w-[100px] h-[3px] justify-self-center'></div>
     </div>
 
     <div>
-      <ul className=' text-gray-200 justify-center my-2 ml-6 flex flex-col  font-semibold'>
-        <li>REACT JS</li>
-        <li>Firebase (Backened Service)</li>
-        <li></li>
+      <ul className=' text-gray-200 justify-center items-center md:ml-4 md:items-start my-2 flex flex-col  font-semibold'>
+        <li>History</li>
+        <li>Our Team</li>
+        <li>Brand Guidelines</li>
+        <li>Terms & Conditions</li>
+        <li>Privacy Policy</li>
       </ul>
     </div>
 
@@ -114,60 +117,59 @@ overflow-hidden  bg-gray-400 border border-t-2 border-t-gray-300'>
   {/* pages */}
 <div className=" flex flex-col w-full mb-3 px-8 md:px-40">
 
-<div className=' flex flex-col justify-center '>
-<h1 className=' justify-self-center text-gray-800 uppercase font-bold text-2xl my-2'>pages</h1>
-<div className=' bg bg-yellow-400 w-[90px] h-[3px] justify-self-center'></div>
-</div>
+<div className=' flex flex-col justify-center items-center'>
+    <h1 className=' justify-self-center text-gray-300 uppercase font-bold text-2xl my-2'>Services</h1>
+    <div className=' bg bg-yellow-400 w-[120px] h-[3px] justify-self-center'></div>
+    </div>
 
 <div>
-  <ul className=' text-gray-200 justify-center my-2 ml-6 flex flex-col  font-semibold'>
-    <li>Home</li>
-    <li>Feeds</li>
-    <li>Login</li>
+  <ul className=' text-gray-200 justify-center items-center md:items-start md:ml-4 my-2 flex flex-col  font-semibold'>
+    <li>Course</li>
+    <li>Blogs</li>
+    <li>Events</li>
+    <li>Contest</li>
+    <li>Payment Method</li>
+
   </ul>
 </div>
 
 </div>
+</div>
 
- {/* help and support */}
+</div>
+
+{/* help and support */}
 <div className=" flex flex-col w-full mb-3 px-8 md:px-40">
 
-<div className=' flex flex-col justify-center '>
-<h1 className=' justify-self-center text-gray-800 uppercase font-bold text-2xl my-2'>Support</h1>
-<div className=' bg bg-yellow-400 w-[130px] h-[3px] justify-self-center'></div>
+<div className=' flex flex-col justify-center items-center '>
+<h1 className=' justify-self-center  text-indigo-500 uppercase font-bold text-2xl my-2'>Geek Grove</h1>
+<h5 className=' justify-self-center text-indigo-500  text-md '>You can visit our social media handles.</h5>
+
 </div>
 
 <div>
-  <ul className=' text-gray-200 justify-center my-2 ml-6 flex flex-col  font-semibold'>
-    <li>[harshdixitofficial@gmail.com]</li>
+  <ul className=' text-gray-200 justify-center my-2 gap-2 flex   font-semibold'>
+    <li><FaSquareFacebook className=' text-blue-300'/></li>
+    <li><FaSquareTwitter className=' text-blue-300'/></li>
+    <li><FaSquareInstagram className=' text-blue-300'/></li>
+    <li><FaSquareYoutube className=' text-blue-300'/></li>
   </ul>
 </div>
 
 </div>
 </div>
 
-</div>
-</div>
-
-{/* left part */}
-<div className=" w-full flex flex-coljustify-center pb-10">
-  
-<div className=' m-auto '>
-
-</div>
-
-</div>
 
 {/* lower part */}
 
-<div className=" bg-gray-500 flex w-full ">
+<div className=" bg-slate-900 flex w-full ">
   <div className=" w-full flex  px-3 py-4 justify-center items-center ">
 
     {/* content */}
 
     <div className=' flex flex-col justify-center items-center ml-10'>
-  <p className=' flex justify-center items-center text-gray-800'>
-    &copy; Copyright 2024
+  <p className=' flex justify-center items-center text-gray-300'>
+  <span className=' font-bold pr-2'>Geek Grove</span> &copy; Copyright 2024
   </p>
   </div>
   </div>
